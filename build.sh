ec815ab14cedc0079469f311c5014d57aa17ac6d
Alexey Shapoval <as@eubclab.com>
2021-06-06 15:58:32 +0300

    Cleaned up unnecessary scripts, config env vars

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd syncer && ./build.sh; cd ..
-cd http-api && ./build.sh; cd ..
-cd webapp && ./build.sh; cd ..
-cd metrics && ./build.sh; cd ..
--- push.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-source env.sh
-docker push councilbox/quorum-explorer-syncer
-docker push councilbox/quorum-explorer-http-api
-docker push councilbox/quorum-explorer-webapp:$WEBAPP_VERSION
